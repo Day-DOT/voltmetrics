c0997a0153c7da9ce07bbd5e9a465c5a541bb657
Day-DOT <al222410870@gmail.com>
2026-03-26 14:18:06 -0600

    Rastreo

--- public/index.php
+++ public/index.php
@@ -1,21 +1,4 @@
 <?php
-
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-// 1. Verificar si la aplicación está en modo mantenimiento
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// 2. Registrar el autoloader de Composer (Sin el ../)
-require __DIR__.'/vendor/autoload.php';
-
-// 3. Inicializar Laravel (Sin el ../)
-/** @var Application $app */
-$app = require_once __DIR__.'/bootstrap/app.php';
-
-// 4. Procesar la petición
-$app->handleRequest(Request::capture());
+echo "<h1>ESTOY EN LA CARPETA PUBLIC</h1>";
+echo "Ruta real: " . __FILE__;
+?>
